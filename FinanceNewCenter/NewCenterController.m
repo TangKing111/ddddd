06a79244469416315ca007fe4ac777be2c51e5e9
汤万珍 <tangwanzhen@tangwanzhendeMacBook-Air.local>
2017-09-12 14:24:10 +0800

    ddddd

--- FinanceNewCenter/NewCenterController.m
+++ FinanceNewCenter/NewCenterController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //dadasd setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
