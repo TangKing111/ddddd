09122d4bd827e9a1c93f21839d579945905bf881
汤万珍 <tangwanzhen@tangwanzhendeMacBook-Air.local>
2017-09-12 14:26:59 +0800

    tangwanzhen

---
+++ 未命名文件夹/ddddViewController.h
@@ -0,0 +1,13 @@
+//
+//  ddddViewController.h
+//  BSFinance
+//
+//  Created by 汤万珍 on 2017/9/12.
+//  Copyright © 2017年 BScompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ddddViewController : UIViewController
+
+@end
---
+++ 未命名文件夹/ddddViewController.m
@@ -0,0 +1,37 @@
+//
+//  ddddViewController.m
+//  BSFinance
+//
+//  Created by 汤万珍 on 2017/9/12.
+//  Copyright © 2017年 BScompany. All rights reserved.
+//
+
+#import "ddddViewController.h"
+
+@interface ddddViewController ()
+
+@end
+
+@implementation ddddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
